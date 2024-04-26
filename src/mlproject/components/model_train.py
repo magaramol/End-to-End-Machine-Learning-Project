b84a7cc3fb9a8a7824c09bd3c19f6eacf3a3f56f
magaramol <amoljb007@gmail.com>
2024-04-26 19:56:59 +0530

    model train

---
+++ src/mlproject/components/model_train.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from dataclasses import dataclass
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+from src.mlproject.utils import save_object,evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join('artifacts','model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
