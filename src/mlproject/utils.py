3f4640bd98771b024d8de685bc7c26afb0ffbd0c
magaramol <amoljb007@gmail.com>
2024-04-26 11:34:52 +0530

    data transformation & utils upadted

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -5,6 +5,8 @@ from src.mlproject.logger import logging
 
 import pandas as pd
 import pymysql
+import pickle
+import numpy
 
 
 
@@ -49,3 +51,15 @@ def read_sql_data():
         #pass
     except Exception as ex:
         raise CustomException(ex)
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
