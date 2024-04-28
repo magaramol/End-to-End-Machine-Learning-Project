ca86e80f09d9fe2446e68d83c48c3a932b609abe
magaramol <amoljb007@gmail.com>
2024-04-28 23:12:00 +0530

    model trainer updated

--- src/mlproject/components/model_train.py
+++ src/mlproject/components/model_train.py
@@ -30,3 +30,9 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
+
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
