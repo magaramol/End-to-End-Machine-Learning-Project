8dc54064962a131137bfbc21446405881ca3d1fd
magaramol <amoljb007@gmail.com>
2024-04-29 11:26:32 +0530

    model_train.py updated

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from src.mlproject.logger import logging
-import sys
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
-from src.mlproject.components.data_transformation import DataTransformation
-from src.mlproject.components.data_transformation import DataTransformationConfig
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_train import ModelTrainerConfig,ModelTrainer
 
-
-
+import sys
 
 if __name__=="__main__":
     logging.info("the exce started")
@@ -16,13 +14,20 @@ if __name__=="__main__":
     try:
         #data_ingestion_config=DataingestionConfig
         data_ingestion=DataIngestion()
-        train_path_data,test_path_data=data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
         #        data_ingestion.initiate_data_ingestion()
        # data_ingestion=DataIngestion() 
         #train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_path_data,test_path_data)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
+        # model training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        #model_trainer.initiate_model_trainer(train_arr,test_arr)
 
 
         
--- src/mlproject/components/model_train.py
+++ src/mlproject/components/model_train.py
@@ -27,21 +27,201 @@ from src.mlproject.utils import save_object,evaluate_models
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join('artifacts','model.pkl')
 
-class ModelTrainer:
-    def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+# class ModelTrainer:
+#     def __init__(self):
+#         self.model_trainer_config=ModelTrainerConfig()
 
 
-    def ModelTrainer(self):
-        def __init__(self,train_array,test_array):
-            try:
-                logging.info("split train & test input data")
+#     def ModelTrainer(self):
+#         def initiate_model_trainer(self,train_array,test_array):
+#             try:
+#                 logging.info("split train & test input data")
+#                 X_train,y_train,X_test,y_test=(
+#                 train_array[:,:-1],
+#                 train_array[:,-1],
+#                 test_array[:,:-1],
+#                 test_array[:,-1]
+#                 )                
                 
-            except Exception as e:
-                raise CustomException(e,sys)
+
+#                 models = {
+#                 "Random Forest": RandomForestRegressor(),
+#                 "Decision Tree": DecisionTreeRegressor(),
+#                 "Gradient Boosting": GradientBoostingRegressor(),
+#                 "Linear Regression": LinearRegression(),
+#                 "XGBRegressor": XGBRegressor(),
+#                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+#                 "AdaBoost Regressor": AdaBoostRegressor(),
+#                  }
+
+#                 params={
+#                 "Decision Tree": {
+#                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+#                     # 'splitter':['best','random'],
+#                     # 'max_features':['sqrt','log2'],
+#                 },
+#                 "Random Forest":{
+#                     # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+#                     # 'max_features':['sqrt','log2',None],
+#                     'n_estimators': [8,16,32,64,128,256]
+#                 },
+#                 "Gradient Boosting":{
+#                     # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+#                     'learning_rate':[.1,.01,.05,.001],
+#                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+#                     # 'criterion':['squared_error', 'friedman_mse'],
+#                     # 'max_features':['auto','sqrt','log2'],
+#                     'n_estimators': [8,16,32,64,128,256]
+#                 },
+#                 "Linear Regression":{},
+#                 "XGBRegressor":{
+#                     'learning_rate':[.1,.01,.05,.001],
+#                     'n_estimators': [8,16,32,64,128,256]
+#                 },
+#                 "CatBoosting Regressor":{
+#                     'depth': [6,8,10],
+#                     'learning_rate': [0.01, 0.05, 0.1],
+#                     'iterations': [30, 50, 100]
+#                 },
+#                 "AdaBoost Regressor":{
+#                     'learning_rate':[.1,.01,0.5,.001],
+#                     # 'loss':['linear','square','exponential'],
+#                     'n_estimators': [8,16,32,64,128,256]
+#                 }
+                
+#                 }
+#                 model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,params)
+#                 ## To get best model score from dict
+#                 best_model_score = max(sorted(model_report.values()))
+
+#                 ## To get best model name from dict
+
+#                 best_model_name = list(model_report.keys())[
+#                 list(model_report.values()).index(best_model_score)
+#                 ]
+#                 best_model = models[best_model_name]
+
+#                 print("This is the best model:")
+#                 print(best_model_name)
+
+
+#                 if best_model_score<0.6:
+#                     raise CustomException("No best model found")
+#                 logging.info(f"Best found model on both training and testing dataset")
+
+#                 save_object(
+#                 file_path=self.model_trainer_config.trained_model_file_path,
+#                 obj=best_model
+#                 )
+#                 predicted=best_model.predict(X_test)
+                
+#                 r2_square = r2_score(y_test, predicted)
+#                 return r2_square
+
+
+
+
+#             except Exception as e:
+#                 raise CustomException(e,sys)
             
 
-    def eval_metrics(self,actual, pred):
+#     def eval_metrics(self,actual, pred):
+#         rmse = np.sqrt(mean_squared_error(actual, pred))
+#         mae = mean_absolute_error(actual, pred)
+#         r2 = r2_score(actual, pred)
+#         return rmse, mae, r2
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("split train & test input data")
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter': ['best', 'random'],
+                    # 'max_features': ['sqrt', 'log2'],
+                },
+                "Random Forest": {
+                    # 'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features': ['sqrt', 'log2', None],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    # 'loss': ['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion': ['squared_error', 'friedman_mse'],
+                    # 'max_features': ['auto', 'sqrt', 'log2'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    # 'loss': ['linear', 'square', 'exponential'],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+
+            }
+            model_report: dict = evaluate_models(X_train, y_train, X_test, y_test, models, params)
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            print("This is the best model:")
+            print(best_model_name)
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def eval_metrics(self, actual, pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
         r2 = r2_score(actual, pred)
